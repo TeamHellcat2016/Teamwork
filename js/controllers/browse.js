009449c52548b36f9c35ebee88125741598817ee
Elena Zarkova <ezarkova@gmail.com>
2016-09-28 16:46:52 +0300

    Added search

--- js/controllers/browse.js
+++ js/controllers/browse.js
@@ -5,6 +5,7 @@ let browse = (function () {
 
     function getBrowsing(context) {
         var data;
+        var templateListFunc;
         requester.getAllRestaurants()
             .then((result) => {
                 data = result;
@@ -14,7 +15,67 @@ let browse = (function () {
                 return templates.get("not-logged-in");
             })
             .then((templateFunc) => {
-                context.$element().html(templateFunc({ data }));
+                context.$element().html(templateFunc());
+
+                templates.get("restaurants-list")
+                    .then((listFunc)=>{
+                        templateListFunc = listFunc;
+                        $("#restaurants-list").html(templateListFunc({data}));
+                    });
+
+                if(data){
+                    $("#search-restaurants").on("click", function(ev){
+                        const city = $("#city").val().toLowerCase();
+                        const type = $("#type").val().toLowerCase();
+                        const cuisine = $("#cuisine").val().toLowerCase();
+                        const name = $("#place-name").val().toLowerCase();
+
+                        $("#city").val("");
+                        $("#type").val("");
+                        $("#cuisine").val("");
+                        $("#place-name").val("");
+
+                        const options = {city, type, cuisine, name};
+
+                        requester.getAllRestaurants()
+                            .then((restaurants) =>{
+                                var sortedRestaurants = [];
+                                restaurants.forEach((restaurant) => {
+                                    var answersConditions = true;
+                                    for (var property in options) {
+                                        if(options[property] === ""){
+                                            continue;
+                                        }
+
+                                        if(Array.isArray(restaurant[property])) {
+                                            var hasMatch = restaurant[property].some((element) => element.toLowerCase() === options[property]);
+                                            if(hasMatch){
+                                                continue;
+                                            }else{
+                                                answersConditions = false;
+                                                break;
+                                            }
+                                        }
+
+                                        if(options[property] !== restaurant[property].toLowerCase()){
+                                            answersConditions = false;
+                                            break;
+                                        }
+                                    }
+
+                                    if(answersConditions){
+                                        sortedRestaurants.push(restaurant);
+                                    }
+                                });
+
+                                $("#restaurants-list").html(templateListFunc({data: sortedRestaurants}));
+
+                            });
+
+                        ev.preventDefault();
+                        return false;
+                    });
+                }
             });
         }
 
