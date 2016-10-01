7ef442b86dd823d4311237b93a4a5e8c46abd8a2
Todor13 <todorstaykovski@gmail.com>
2016-10-02 00:51:54 +0300

    Buttons in user favourites are ready

--- js/controllers/user-profile.js
+++ js/controllers/user-profile.js
@@ -45,7 +45,48 @@ let userProfile = (function () {
                         $(".add-to-favourites").addClass("hidden");
                     })
                     .then(()=>{
+                        let parent;
+                        let description;
+                        $(".list-unstyled").on("click", ".btn-info", function (ev) {
+                            parent = $(ev.target).parents('.restaurant');
+                            if(parent.children().children('.desc').children().length == 0){
+                                let id = parent.attr("data-id");
+                                requester.getRestaurantById(id)
+                                    .then((res)=>{
+                                        description = res.description;
+                                        return templates.get('description');
+                                    })
+                                    .then((templateFunc)=>{
+                                        parent.children().children('.desc').append(templateFunc(description));
+                                    })
+                            } else {
+                                parent.children().children('.desc').children().remove('#restaurant-description');
+                            }
+                            ev.preventDefault();
+                            return false;
+                        });
 
+                        let comment;
+                        let parentComment;
+                        $(".list-unstyled").on("click", ".btn-comments", function (ev){
+                            parentComment = $(ev.target).parents('.restaurant');
+
+                            if(parentComment.children().children('.comments').children().length == 0){
+                                var id = parentComment.attr('data-id');
+                                requester.getRestaurantById(id)
+                                    .then((res)=>{
+                                        comment = res.comments;
+                                        return templates.get('comment');
+                                    })
+                                    .then((templateFunc)=>{
+                                        parentComment.children().children('.comments').append(templateFunc(comment));
+                                    })
+                            } else {
+                                parentComment.children().children('.comments').children().remove('.restaurant-comment');
+                            }
+                            ev.preventDefault();
+                            return false;
+                        });
                     })
             });
     }
