4788173942f02bda1b09776a440e06bc84f9dbbe
Panglot <DobromirBogatev@gmail.com>
2016-10-03 00:21:28 +0300

    Map cleaning up

--- js/controllers/map.js
+++ js/controllers/map.js
@@ -6,50 +6,48 @@ let map = (function () {
     function getMap(context) {
         templates.get("map")
             .then((templateFunc) => {
-                context.$element().html(templateFunc());              
-                        var map;
-                        var infowindow;
-                      
-                        function initMap() {
-                            
-                            var pyrmont = {lat: 42.6492994, lng: 23.3723664};
+                context.$element().html(templateFunc());
+                var map;
+                var infowindow;
 
-                            map = new google.maps.Map(document.getElementById('map'), {
-                            center: pyrmont,
-                            zoom: 16
-                            });
+                function initMap() {
+                    var pyrmont = {lat: 42.6492994, lng: 23.3723664};
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: pyrmont,
+                        zoom: 16
+                    });
 
-                            infowindow = new google.maps.InfoWindow();
-                            var service = new google.maps.places.PlacesService(map);
-                            service.nearbySearch({
-                            location: pyrmont,
-                            radius: 500,
-                            type: ['restaurant']
-                            }, callback);
+                    infowindow = new google.maps.InfoWindow();
+                    var service = new google.maps.places.PlacesService(map);
+                    service.nearbySearch({
+                        location: pyrmont,
+                        radius: 500,
+                        type: ['restaurant']
+                    }, callback);
+                }
+
+                function callback(results, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        for (var i = 0; i < results.length; i++) {
+                            createMarker(results[i]);
                         }
+                    }
+                }
 
-                        function callback(results, status) {
-                            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                            for (var i = 0; i < results.length; i++) {
-                                createMarker(results[i]);
-                            }
-                            }
-                        }
+                function createMarker(place) {
+                    var placeLoc = place.geometry.location;
+                    var marker = new google.maps.Marker({
+                        map: map,
+                        position: place.geometry.location
+                    });
 
-                        function createMarker(place) {
-                            var placeLoc = place.geometry.location;
-                            var marker = new google.maps.Marker({
-                            map: map,
-                            position: place.geometry.location
-                            });
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infowindow.setContent(place.name);
+                        infowindow.open(map, this);
+                    });
+                }
 
-                            google.maps.event.addListener(marker, 'click', function() {
-                            infowindow.setContent(place.name);
-                            infowindow.open(map, this);
-                            });
-                        }
-
-                        initMap()
+                initMap()
             });
 
     }
@@ -57,6 +55,6 @@ let map = (function () {
     return {
         all: getMap
     };
-} ());
+}());
 
 export {map};
--- js/helpers/map.js
+++ js/helpers/map.js
@@ -1,5 +1,4 @@
     var map;
-    var infowindow;
 
     function initMap() {
         var pyrmont = {lat: 42.6492994, lng: 23.3723664};
@@ -8,7 +7,6 @@
             zoom: 16
         });
 
-        infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
             location: pyrmont,
@@ -24,32 +22,3 @@
             }
         }
     }
-
-    function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-        });
-    }
-
-    // mapLink.addEventListener("click", ()=> {
-    //     if (mapDiv.style.display === 'none') {
-    //
-    //         mapDiv.style.display = 'block';
-    //         initMap();
-    //
-    //     }
-    //     else {
-    //         mapDiv.style.display = 'none';
-    //
-    //     }
-    // })
-
-    //initMap();
-
